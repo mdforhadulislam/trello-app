a38149a279eafa200b265c23643624e431ffb437
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-08-31 15:33:06 +0600

    add file

--- controllers/boards/putBoardHendler.js
+++ controllers/boards/putBoardHendler.js
@@ -2,19 +2,86 @@ const curd = require("../../lib/curdOparations");
 
 const putBoardHendler = (req, res) => {
   try {
+    // get to query board id
     const { id } = req.params;
-    curd.read("boards", (err, data) => {
-      if (err) {
-        const datas = JSON.parse(data);
-        const findBoard = datas.find((board) => board.id === id);
-        if (findBoard) {
+
+    // get board updatet data
+    const { name, color } = req.body;
+
+    // get database to recive all board data
+    curd.read(
+      "boards",
+      /**
+       *
+       * @param {boolean} err
+       * @param {Array} data
+       */
+      (err, data) => {
+        if (err) {
+          // convart data json to array
+          const datas = JSON.parse(data);
+
+          // filter to data and update main object
+          const findBoard = datas.find(
+            /**
+             *
+             * @param {object} board
+             * @returns {object}object
+             */
+            (board) => board.id === id
+          );
+
+          if (findBoard) {
+            const newBoard = {
+              ...findBoard,
+              name,
+              color,
+            };
+
+            // delete to database old data
+            curd.delete(
+              "boards",
+              newBoard.id,
+              /**
+               *
+               * @param {boolean} err
+               * @param {Array} data
+               */
+              (err, data) => {
+                // if err true
+                if (err) {
+                  // create new data object in database
+                  curd.create(
+                    "boards",
+                    newBoard,
+                    /**
+                     *
+                     * @param {boolean} err
+                     * @param {Array} data
+                     */
+                    (err, data) => {
+                      if (err) {
+                        res.status(200).json(newBoard);
+                      } else {
+                        res
+                          .status(500)
+                          .json({ message: "Internal Server Error" });
+                      }
+                    }
+                  );
+                } else {
+                  res.status(500).json({ message: "Internal Server Error" });
+                }
+              }
+            );
+          } else {
+            res.status(404).json({ message: "board not found" });
+          }
         } else {
+          res.status(500).json({ message: "Internal Server Error" });
         }
-        res.status(404).json({ message: "board not found" });
-      } else {
-        res.status(500).json({ message: "Internal Server Error" });
       }
-    });
+    );
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }
