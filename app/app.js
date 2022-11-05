73156c004d44c873452bfa2ad45897bcf5205229
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2022-11-05 11:07:53 +0600

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(require("./middlewares"));
-app.use("/", require("./router"));
+const middlewares = require("./middlewares")
+const routers=require("./router")
+
+app.use(middlewares);
+app.use(routers);
 
 module.exports = app;
